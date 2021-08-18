873ec0abb4d8d673dae01f0056a0ca4316a6a0bf
sakiso <>
2021-08-18 09:25:14 +0900

    Xpathを定数化

--- app/controllers/api/fumoto/v1/fumoto_controller.rb
+++ app/controllers/api/fumoto/v1/fumoto_controller.rb
@@ -5,6 +5,18 @@ class Api::Fumoto::V1::FumotoController < ActionController::API
     require 'uri'
     require 'nokogiri'
 
+    # 10月の土曜日のXpath定義
+    xpath_10_02 =
+      '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[3]/td[3]'
+    xpath_10_09 =
+      '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[10]/td[3]'
+    xpath_10_16 =
+      '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[17]/td[3]'
+    xpath_10_23 =
+      '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[24]/td[3]'
+    xpath_10_30 =
+      '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[31]/td[3]'
+
     # ふもとっぱら予約状況のURLにPOSTして予約状況のレスポンス取得
     uri = URI.parse('https://fumotoppara.secure.force.com/RS_Top')
     request = Net::HTTP::Post.new(uri)
@@ -21,13 +33,9 @@ class Api::Fumoto::V1::FumotoController < ActionController::API
 
     # nokogiriでパース
     doc = Nokogiri.HTML(response.body)
-    reservation_html =
-      doc.xpath(
-        '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[17]/td[3]',
-      )
 
-    reservation_status = reservation_checker(reservation_html)
-
+    # 予約状況を判定しJSONで返却
+    reservation_status = reservation_checker(doc.xpath(xpath_10_16))
     render json: { reservation_status: reservation_status }
   end
 
