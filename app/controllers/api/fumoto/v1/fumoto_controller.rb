c8fd2aaebb8ea268cef7c52d2446ba5a081e6c6b
sakiso <>
2021-08-19 23:09:41 +0900

    ふもとっぱらのスクレイピング完成

--- app/controllers/api/fumoto/v1/fumoto_controller.rb
+++ app/controllers/api/fumoto/v1/fumoto_controller.rb
@@ -7,15 +7,15 @@ class Api::Fumoto::V1::FumotoController < ActionController::API
 
     # 10月の土曜日のXpath定義
     xpaths = {
-      xpath_10_02:
+      date_10_02:
         '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[3]/td[3]',
-      xpath_10_09:
+      date_10_09:
         '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[10]/td[3]',
-      xpath_10_16:
+      date_10_16:
         '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[17]/td[3]',
-      xpath_10_23:
+      date_10_23:
         '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[24]/td[3]',
-      xpath_10_30:
+      date_10_30:
         '/html/body/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[31]/td[3]',
     }
 
@@ -28,6 +28,8 @@ class Api::Fumoto::V1::FumotoController < ActionController::API
       'j_id0:fSearch:searchBtn' => '検索',
     )
     req_options = { use_ssl: uri.scheme == 'https' }
+
+    # スクレイピング実行
     response =
       Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
@@ -36,11 +38,17 @@ class Api::Fumoto::V1::FumotoController < ActionController::API
     # nokogiriでパース
     doc = Nokogiri.HTML(response.body)
 
-    # 予約状況を判定しJSONで返却
-    reservation_status = reservation_checker(doc.xpath(xpaths[:xpath_10_16]))
-    render json: { reservation_status: reservation_status }
+    # 予約状況を判定しハッシュ形式で返却
+    reservation_status = {}
+    xpaths.each do |key, value|
+      reservation_status.store(key, reservation_checker(doc.xpath(value)))
+    end
+
+    # JSON出力
+    render json: reservation_status
   end
 
+  # 与えられたHTML要素に予約状況を示す記号が含まれていればその記号を返す
   def reservation_checker(reservation_html)
     if reservation_html.to_s.include?('○')
       status = '○'
