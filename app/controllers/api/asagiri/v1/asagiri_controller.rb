54758335b51a181d1cea75cfee84d83be6fc1411
sakiso <>
2021-08-20 00:03:00 +0900

    朝霧の予約状況スクレイピング完成＋テスト追加

--- app/controllers/api/asagiri/v1/asagiri_controller.rb
+++ app/controllers/api/asagiri/v1/asagiri_controller.rb
@@ -5,14 +5,53 @@ class Api::Asagiri::V1::AsagiriController < ActionController::API
     require 'uri'
     require 'nokogiri'
 
+    # 10月の土曜日のXPath定義
+    xpaths = {
+      date_10_02: '/html/body/div[2]/div/div[1]/div/table[3]/tr[2]/td[1]',
+      date_10_09: '/html/body/div[2]/div/div[1]/div/table[3]/tr[2]/td[2]',
+      date_10_16: '/html/body/div[2]/div/div[1]/div/table[3]/tr[2]/td[3]',
+      date_10_23: '/html/body/div[2]/div/div[1]/div/table[3]/tr[2]/td[4]',
+      date_10_30: '/html/body/div[2]/div/div[1]/div/table[3]/tr[2]/td[5]',
+    }
+
+    # 朝霧予約状況のURLにGETする準備
     url = URI.parse('https://www.asagiri-camp.net/reservation.html')
     request = Net::HTTP::Get.new(url.path)
     req_options = { use_ssl: url.scheme == 'https' }
+
+    # GETしてスクレイピング実行
     response =
       Net::HTTP.start(url.host, url.port, req_options) do |http|
         http.request(request)
       end
 
-    render json: { stub: 'this is stub' }
+    # nokogiriでパース
+    doc = Nokogiri.HTML(response.body)
+
+    # 予約状況を判定しハッシュ形式で返却
+    reservation_status = {}
+    xpaths.each do |key, value|
+      reservation_status.store(key, reservation_checker(doc.xpath(value)))
+    end
+
+    # JSON出力
+    render json: reservation_status
+  end
+
+  # 与えられたHTML要素に予約状況を示す記号が含まれていればその記号を返す
+  # ○と×は似た別の文字があるので表記ゆれ対策している
+  def reservation_checker(reservation_html)
+    html_str = reservation_html.to_s
+
+    if html_str.include?('○') || html_str.include?('◯')
+      status = '○'
+    elsif html_str.include?('△')
+      status = '△'
+    elsif html_str.include?('✕') || html_str.include?('×')
+      status = '×'
+    else
+      status = nil
+    end
+    return status
   end
 end
--- test/controllers/asagiri_controller_test.rb
+++ test/controllers/asagiri_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class AsagiriControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test '朝霧ジャンボリーの予約状況を取得する' do
+    get api_asagiri_v1_check_reservation_url
+    assert_not JSON.parse(response.body) == nil
+    assert response.status == 200
+  end
 end
