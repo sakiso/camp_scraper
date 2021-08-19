7a53261713b22e4169707eed3f44ca8c68e86238
sakiso <>
2021-08-19 23:35:13 +0900

    とりあえずGETリクエストが正常終了する状態

--- app/controllers/api/asagiri/v1/asagiri_controller.rb
+++ app/controllers/api/asagiri/v1/asagiri_controller.rb
@@ -1,5 +1,18 @@
 class Api::Asagiri::V1::AsagiriController < ActionController::API
   def index
+    # ライブラリのインポート
+    require 'net/http'
+    require 'uri'
+    require 'nokogiri'
+
+    url = URI.parse('https://www.asagiri-camp.net/reservation.html')
+    request = Net::HTTP::Get.new(url.path)
+    req_options = { use_ssl: url.scheme == 'https' }
+    response =
+      Net::HTTP.start(url.host, url.port, req_options) do |http|
+        http.request(request)
+      end
+
     render json: { stub: 'this is stub' }
   end
 end
